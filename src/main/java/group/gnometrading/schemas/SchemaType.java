6d966a16c5e07ee6879a4435f916bfb1a6e6f959
mprey <masonprey7@gmail.com>
2025-04-01 08:28:07 -0400

    feat(schema): add a singleton instance to enum

--- src/main/java/group/gnometrading/schemas/SchemaType.java
+++ src/main/java/group/gnometrading/schemas/SchemaType.java
@@ -1,25 +1,31 @@
 package group.gnometrading.schemas;
 
 public enum SchemaType {
-    MBO("mbo"),
-    MBP_10("mbp-10"),
-    MBP_1("mbp-1"),
-    BBO_1S("bbo-1s"),
-    BBO_1M("bbo-1m"),
-    TRADES("trades"),
-    OHLCV_1S("ohlcv-1s"),
-    OHLCV_1M("ohlcv-1m"),
-    OHLCV_1H("ohlcv-1h"),
+    MBO("mbo", new MBP10Schema()),
+    MBP_10("mbp-10", new MBP10Schema()),
+    MBP_1("mbp-1", new MBP1Schema()),
+    BBO_1S("bbo-1s", new BBO1SSchema()),
+    BBO_1M("bbo-1m", new BBO1MSchema()),
+    TRADES("trades", new TradesSchema()),
+    OHLCV_1S("ohlcv-1s", new OHLCV1SSchema()),
+    OHLCV_1M("ohlcv-1m", new OHLCV1MSchema()),
+    OHLCV_1H("ohlcv-1h", new OHLCV1HSchema()),
     ;
 
     private final String identifier;
+    private final Schema<?, ?> instance;
 
-    SchemaType(String identifier) {
+    SchemaType(String identifier, Schema<?, ?> instance) {
         this.identifier = identifier;
+        this.instance = instance;
     }
 
     public String getIdentifier() {
         return this.identifier;
+    }
+
+    public Schema<?, ?> getInstance() {
+        return this.instance;
     }
 
     public static SchemaType findById(final String id) {
