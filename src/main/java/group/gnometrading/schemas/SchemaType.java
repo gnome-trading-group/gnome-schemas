f6807c4acd76ce71b968c2b08952414069fbc914
mprey <masonprey7@gmail.com>
2025-12-10 16:20:46 -0500

    feat(*): add a supplier to produce a new instance

--- src/main/java/group/gnometrading/schemas/SchemaType.java
+++ src/main/java/group/gnometrading/schemas/SchemaType.java
@@ -1,23 +1,27 @@
 package group.gnometrading.schemas;
 
+import java.util.function.Supplier;
+
 public enum SchemaType {
-    MBO("mbo", new MBP10Schema()),
-    MBP_10("mbp-10", new MBP10Schema()),
-    MBP_1("mbp-1", new MBP1Schema()),
-    BBO_1S("bbo-1s", new BBO1SSchema()),
-    BBO_1M("bbo-1m", new BBO1MSchema()),
-    TRADES("trades", new TradesSchema()),
-    OHLCV_1S("ohlcv-1s", new OHLCV1SSchema()),
-    OHLCV_1M("ohlcv-1m", new OHLCV1MSchema()),
-    OHLCV_1H("ohlcv-1h", new OHLCV1HSchema()),
+    MBO("mbo", new MBP10Schema(), MBOSchema::new),
+    MBP_10("mbp-10", new MBP10Schema(), MBP10Schema::new),
+    MBP_1("mbp-1", new MBP1Schema(), MBP1Schema::new),
+    BBO_1S("bbo-1s", new BBO1SSchema(), BBO1SSchema::new),
+    BBO_1M("bbo-1m", new BBO1MSchema(), BBO1MSchema::new),
+    TRADES("trades", new TradesSchema(), TradesSchema::new),
+    OHLCV_1S("ohlcv-1s", new OHLCV1SSchema(), OHLCV1SSchema::new),
+    OHLCV_1M("ohlcv-1m", new OHLCV1MSchema(), OHLCV1MSchema::new),
+    OHLCV_1H("ohlcv-1h", new OHLCV1HSchema(), OHLCV1HSchema::new),
     ;
 
     private final String identifier;
     private final Schema instance;
+    private final Supplier<Schema> instanceSupplier;
 
-    SchemaType(String identifier, Schema instance) {
+    SchemaType(String identifier, Schema instance, Supplier<Schema> instanceSupplier) {
         this.identifier = identifier;
         this.instance = instance;
+        this.instanceSupplier = instanceSupplier;
     }
 
     public String getIdentifier() {
@@ -26,6 +30,10 @@ public enum SchemaType {
 
     public Schema getInstance() {
         return this.instance;
+    }
+
+    public Schema newInstance() {
+        return this.instanceSupplier.get();
     }
 
     public static SchemaType findById(final String id) {
